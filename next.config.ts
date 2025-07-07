84fb4dbcd467713bf2086088ebfe7dc6b81f9d0d
Ryan Hofmann <ryanhofmann@Ryans-MacBook-Pro.local>
2025-07-07 15:32:23 -0700

    fix: add GitHub avatars hostname to Next.js image config
    - Allows avatars.githubusercontent.com in remotePatterns
    - Fixes Next.js Image component error for GitHub profile images
    - Enables proper loading of user avatars in Navigation component

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   
